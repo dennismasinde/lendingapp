089f8cec10ae24a1482f5e3ee4480272b163d823
dennismasinde <chengwadennismasinde@gmail.com>
2026-06-30 14:36:17 +0300

    Initial commit

---
+++ src/main/java/com/tezzasolutions/lendingapp/LendingappApplication.java
@@ -0,0 +1,13 @@
+package com.tezzasolutions.lendingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LendingappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LendingappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tezzasolutions/lendingapp/LendingappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tezzasolutions.lendingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LendingappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
